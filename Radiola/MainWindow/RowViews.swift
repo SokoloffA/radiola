8eb2359f025afa055a8490b8a25c3ec676d5d873
Alexander Sokolov <sokoloff.a@gmail.com>
2023-12-03 23:43:37 +0300

    Show votes and quality info for the internet stations

--- Radiola/MainWindow/RowViews.swift
+++ Radiola/MainWindow/RowViews.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct InternetStationRow: View {
     var station: InternetStation
+    let normalFont = Font.system(size: 11)
+    let smallFont = Font.system(size: 10)
 
     /* ****************************************
      *
@@ -30,12 +32,75 @@ struct InternetStationRow: View {
                     .font(.caption)
                     .foregroundColor(.secondary)
 
-                Text("32 k")
-                    .font(.caption)
-                    .foregroundColor(.secondary)
+                Text(votesInfo()).foregroundColor(.secondary)
+
+                Text(qualityInfo()).foregroundColor(.secondary)
+
             }.padding(EdgeInsets(top: 0, leading: 2, bottom: 1, trailing: 8))
         }
     } // body
+
+    /* ****************************************
+     *
+     * ****************************************/
+    private func votesInfo() -> AttributedString {
+        guard let votes = station.votes else { return AttributedString("") }
+
+        var res = AttributedString()
+
+        switch votes {
+            case 0:
+                res.append(format("no votes", normalFont))
+
+            case 0 ..< 1000:
+                res.append(format("votes:", smallFont))
+                res.append(format(" \(votes)", normalFont))
+
+            case 1000 ..< 1_000_000:
+                res.append(format("votes:", smallFont))
+                res.append(format(" \(votes / 1000)", normalFont))
+                res.append(format("k", smallFont))
+            default:
+                res.append(format("votes: ", smallFont))
+                res.append(format("\(votes / 10_000_000)", normalFont))
+                res.append(format("M", smallFont))
+        }
+        return res
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    private func qualityInfo() -> AttributedString {
+        var res = AttributedString()
+
+        if let codec = station.codec {
+            res.append(format("codec: ", smallFont))
+            res.append(format(codec.lowercased(), normalFont))
+        }
+
+        if let bitrate = station.bitrate {
+            switch bitrate {
+                case 0: break
+
+                case 1 ..< 1024:
+                    res.append(format(" \(bitrate)b", normalFont))
+
+                default:
+                    res.append(format(" \(bitrate / 1024)k", normalFont))
+            }
+        }
+        return res
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    private func format(_ str: String, _ font: Font) -> AttributedString {
+        var res = AttributedString(str)
+        res.font = font
+        return res
+    }
 }
 
 // MARK: - Controls
