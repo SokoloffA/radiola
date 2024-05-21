66af6a6aeb3d46422e113514ca9714fc8dc6ce9c
Alexander Sokolov <sokoloff.a@gmail.com>
2024-05-21 16:41:03 +0300

    Move LocalStationList to a separate file

---
+++ Radiola/Stations/LocalStationList.swift
@@ -0,0 +1,251 @@
+//
+//  LocalStationList.swift
+//  Radiola
+//
+//  Created by Alex Sokolov on 21.05.2024.
+//
+
+import Foundation
+
+// MARK: - LocalStationList
+
+class LocalStationList: ObservableObject, StationList {
+    let id = UUID()
+    let title: String
+    let icon: String
+    let help: String?
+    private(set) var file: URL?
+    let root = LocalStationGroup(title: "")
+    var items: [Item] { return root.items }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    init(title: String, icon: String, help: String? = nil) {
+        self.title = title
+        self.icon = icon
+        self.help = help
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func favoritesStations() -> [Station] {
+        return filterStations { $0.isFavorite }
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    private func filterStations(where match: (LocalStation) -> Bool) -> [Station] {
+        var res: [Station] = []
+        var queue: [Item] = items
+
+        while !queue.isEmpty {
+            let item = queue.removeFirst()
+
+            switch item {
+                case let .station(station: station):
+                    if match(station) {
+                        res.append(station)
+                    }
+
+                case let .group(group: group):
+                    queue.insert(contentsOf: group.items, at: 0)
+            }
+        }
+
+        return res
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func first(where predicate: (Station) -> Bool) -> Station? {
+        var queue: [Item] = root.items
+
+        while !queue.isEmpty {
+            let item = queue.removeFirst()
+
+            switch item {
+                case let .station(station: station):
+                    if predicate(station) {
+                        return station
+                    }
+
+                case let .group(group: group):
+                    queue += group.items
+            }
+        }
+
+        return nil
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func item(byID: UUID) -> Item? {
+        var queue: [Item] = root.items
+
+        while !queue.isEmpty {
+            let item = queue.removeFirst()
+            if item.id == byID {
+                return item
+            }
+
+            switch item {
+                case let .group(group: group): queue += group.items
+                default: break
+            }
+        }
+
+        return nil
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func append(_ station: LocalStation) {
+        root.append(station)
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func append(_ group: LocalStationGroup) {
+        root.append(group)
+    }
+}
+
+extension [LocalStationList] {
+    func find(byId: UUID) -> LocalStationList? {
+        return first { $0.id == byId }
+    }
+}
+
+extension LocalStationList {
+    /* ****************************************
+     *
+     * ****************************************/
+    func load(file: URL, defaultStations: [LocalStation] = []) throws {
+        self.file = file
+
+        if !FileManager().fileExists(atPath: file.path) {
+            throw Alarm(title: "Can't read the stations file", message: "File doesn't exist")
+        }
+
+        func getBoolAttribute(xml: XMLElement, attribute: String) -> Bool {
+            return (xml.attribute(forName: attribute)?.stringValue ?? "").uppercased() == "TRUE"
+        }
+
+        func loadOutline(xml: XMLElement, parent: LocalStationGroup) {
+            let children = xml.elements(forName: "outline")
+
+            if getBoolAttribute(xml: xml, attribute: "group") || !children.isEmpty {
+                let group = LocalStationGroup(title: xml.attribute(forName: "text")?.stringValue ?? "")
+
+                for outline in children {
+                    loadOutline(xml: outline, parent: group)
+                }
+                parent.append(group)
+                return
+            }
+
+            parent.append(LocalStation(
+                title: xml.attribute(forName: "text")?.stringValue ?? "",
+                url: xml.attribute(forName: "url")?.stringValue ?? "",
+                isFavorite: getBoolAttribute(xml: xml, attribute: "fav")
+            ))
+        }
+
+        do {
+            let xml = try XMLDocument(contentsOf: file)
+            guard let xmlRoot = xml.rootElement() else { return }
+            let xmlBody = xmlRoot.elements(forName: "body")
+            if xmlBody.isEmpty {
+                return
+            }
+
+            for outline in xmlBody[0].elements(forName: "outline") {
+                loadOutline(xml: outline, parent: root)
+            }
+        } catch {
+            throw Alarm(title: "Can't read the stations file", message: "The file is corrupted or has an incorrect format", parentError: error)
+        }
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func save() {
+        guard let file = file else { return }
+        saveAs(file: file)
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func saveAs(file: URL) {
+        func writeOutline(parent: XMLElement, item: Item) {
+            let outline = XMLElement(name: "outline")
+            outline.addAttribute(XMLNode.attribute(withName: "text", stringValue: item.title) as! XMLNode)
+
+            switch item {
+                case let .station(station: station):
+                    outline.addAttribute(XMLNode.attribute(withName: "url", stringValue: station.url) as! XMLNode)
+                    if station.isFavorite {
+                        outline.addAttribute(XMLNode.attribute(withName: "fav", stringValue: "true") as! XMLNode)
+                    }
+
+                case let .group(group: group):
+                    outline.addAttribute(XMLNode.attribute(withName: "group", stringValue: "true") as! XMLNode)
+                    for it in group.items {
+                        writeOutline(parent: outline, item: it)
+                    }
+            }
+
+            parent.addChild(outline)
+        }
+
+        let ompl = XMLElement(name: "ompl")
+        ompl.addAttribute(XMLNode.attribute(withName: "version", stringValue: "2.0") as! XMLNode)
+        ompl.addChild(XMLElement(name: "head"))
+        let body = XMLElement(name: "body")
+        ompl.addChild(body)
+
+        for item in root.items {
+            writeOutline(parent: body, item: item)
+        }
+
+        do {
+            let document = XMLDocument(rootElement: ompl)
+            let xmlData = document.xmlData(options: .nodePrettyPrint)
+            try xmlData.write(to: file)
+        } catch {
+        }
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func dump() {
+        func dump(_ item: Item, indent: String) {
+            switch item {
+                case let .station(station: station):
+                    print("\(indent)○ [\(station.id)] \(station.title) \(station.url)")
+                    return
+
+                case let .group(group: group):
+                    print("\(indent)▼ [\(group.id)] \(group.title)")
+
+                    for item in group.items {
+                        dump(item, indent: "  " + indent)
+                    }
+            }
+        }
+
+        for item in root.items {
+            dump(item, indent: "")
+        }
+    }
+}
