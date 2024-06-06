6979d4105c949433615c6d05b9271b0b85cb32e2
Alexander Sokolov <sokoloff.a@gmail.com>
2024-06-06 22:18:58 +0300

    Use new visual style for "Add station" and "Remove station" buttons.

--- Radiola/StationsWindow/LocalStations/LocalStationToolBox.swift
+++ Radiola/StationsWindow/LocalStations/LocalStationToolBox.swift
@@ -25,28 +25,31 @@ class LocalStationToolBox: NSView {
 
         addButton.bezelStyle = .smallSquare
         addButton.setButtonType(.momentaryPushIn)
-        addButton.image = NSImage(named: NSImage.addTemplateName)
+        addButton.image = NSImage(systemSymbolName: NSImage.Name("plus.circle"), accessibilityDescription: "Add station")
+        addButton.title = "Add station"
+        addButton.imagePosition = .imageLeft
         addButton.image?.isTemplate = true
+        addButton.isBordered = false
 
         delButton.bezelStyle = .smallSquare
         delButton.setButtonType(.momentaryPushIn)
-        delButton.image = NSImage(named: NSImage.removeTemplateName)
+        delButton.image = NSImage(systemSymbolName: NSImage.Name("minus.circle"), accessibilityDescription: "Delete station")
+        delButton.title = "Remove station"
+        delButton.imagePosition = .imageLeft
         delButton.image?.isTemplate = true
+        delButton.isBordered = false
 
         addButton.translatesAutoresizingMaskIntoConstraints = false
         delButton.translatesAutoresizingMaskIntoConstraints = false
 
         addButton.heightAnchor.constraint(equalToConstant: 24).isActive = true
-        addButton.widthAnchor.constraint(equalToConstant: 24).isActive = true
+        delButton.heightAnchor.constraint(equalTo: addButton.heightAnchor).isActive = true
 
         addButton.topAnchor.constraint(equalTo: topAnchor, constant: 6).isActive = true
-        addButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
-
-        delButton.heightAnchor.constraint(equalTo: addButton.heightAnchor).isActive = true
-        delButton.widthAnchor.constraint(equalTo: addButton.widthAnchor).isActive = true
-
         delButton.topAnchor.constraint(equalTo: addButton.topAnchor).isActive = true
-        delButton.leadingAnchor.constraint(equalTo: addButton.trailingAnchor).isActive = true
+
+        addButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16).isActive = true
+        delButton.leadingAnchor.constraint(equalToSystemSpacingAfter: addButton.trailingAnchor, multiplier: 1).isActive = true
     }
 
     /* ****************************************
