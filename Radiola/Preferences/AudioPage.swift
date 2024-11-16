be131ae2bed650a269f362e883a3923e6ee233a0
Alexander Sokolov <sokoloff.a@gmail.com>
2024-11-17 02:17:44 +0300

    Translations

--- Radiola/Preferences/AudioPage.swift
+++ Radiola/Preferences/AudioPage.swift
@@ -13,6 +13,21 @@ class AudioPage: NSViewController {
     /* ****************************************
      *
      * ****************************************/
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        title = NSLocalizedString("Audio", tableName: "Settings", comment: "Settings tab title")
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
     override func viewDidLoad() {
         super.viewDidLoad()
 
