42ecba687e6d3af1509a3ffb14d892bc4ff52fe6
Alexander Sokolov <sokoloff.a@gmail.com>
2024-12-30 14:22:58 +0300

    [CI] Use .dmg file for notarization

--- .github/workflows/makedmg.sh
+++ .github/workflows/makedmg.sh
@@ -2,7 +2,6 @@
 # v 0.1
 
 APP_NAME=Radiola
-MAKE_DMG=true
 CERT_IDENTITY="Developer ID Application: Alex Sokolov (635H9TYSZJ)"
 KEYCHAIN_PROFILE="NotaryTool"
 
@@ -25,12 +24,11 @@ tar xf ${TAR}
 
 VERSION=$(/usr/libexec/PlistBuddy -c "Print :CFBundleVersion" ${BUNDLE_PATH}/Contents/Info.plist)
 DMG_NAME="./${APP_NAME}-${VERSION}.dmg"
-ZIP_NAME="./${APP_NAME}-${VERSION}.zip"
 
 echo "***********************************"
 echo "* App     ${APP_NAME}"
 echo "* Version ${VERSION}"
-echo "* DMG     ${MAKE_DMG}"
+echo "* DMG     ${DMG_NAME}"
 echo "***********************************"
 
 echo "Remove quarantine flag ........................"
@@ -48,17 +46,14 @@ spctl --assess --type execute "${BUNDLE_PATH}"
 echo "  OK"
 echo ""
 
+echo "Create DMG file .............................."
+dmgbuild -s dmg_settings.json "${BUNDLE_PATH}" "${DMG_NAME}"
+echo "  OK"
+
 echo " Notarize ....................................."
-ditto -c -k --keepParent "${BUNDLE_PATH}" "${ZIP_NAME}"
-xcrun notarytool submit --wait --keychain-profile "${KEYCHAIN_PROFILE}" "${ZIP_NAME}" | tee notarytool-submit.log
+xcrun notarytool submit --wait --keychain-profile "${KEYCHAIN_PROFILE}" "${DMG_NAME}" | tee notarytool-submit.log
 echo "  OK"
 echo ""
 
 echo "Verify notarization ..........................."
 spctl -a -vvv -t install "${BUNDLE_PATH}"
-
-if [[ "$MAKE_DMG" == "true" ]]; then
-    echo "Create DMG file .............................."
-    dmgbuild -s dmg_settings.json "${BUNDLE_PATH}" "${DMG_NAME}"
-    echo "  OK"
-fi
