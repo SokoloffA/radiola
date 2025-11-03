0fa7100e68a35c741949b91cbe410b50ca05a8da
Alexander Sokolov <sokoloff.a@gmail.com>
2025-11-03 12:53:40 +0300

    Close issue #82: Feature Request: Notification instead of error popup

---
+++ Radiola/NotificationManager.swift
@@ -0,0 +1,78 @@
+//
+//  NotificationManager.swift
+//  Radiola
+//
+//  Created by Alex Sokolov on 03.11.2025.
+//
+
+import Foundation
+import UserNotifications
+
+final class NotificationManager: NSObject {
+    static let shared = NotificationManager()
+
+    /* ****************************************
+     *
+     * ****************************************/
+    override private init() {
+        super.init()
+        UNUserNotificationCenter.current().delegate = self
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func requestAuthorization(completion: @escaping (Bool) -> Void) {
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            if let err = error {
+                debug("Notification auth error: %@", String(describing: err))
+            }
+
+            DispatchQueue.main.async {
+                completion(granted)
+            }
+        }
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+    func postNotification(title: String, body: String, userInfo: [AnyHashable: Any]? = nil, identifier: String = UUID().uuidString) {
+        let content = UNMutableNotificationContent()
+        content.body = title + "\n" + body
+
+        if let userInfo = userInfo {
+            content.userInfo = userInfo
+        }
+
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 0.1, repeats: false)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request)
+    }
+}
+
+// MARK: - UNUserNotificationCenterDelegate
+
+extension NotificationManager: UNUserNotificationCenterDelegate {
+    /* ****************************************
+     *
+     * ****************************************/
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound])
+    }
+
+    /* ****************************************
+     *
+     * ****************************************/
+//    func userNotificationCenter(_ center: UNUserNotificationCenter,
+//                                didReceive response: UNNotificationResponse,
+//                                withCompletionHandler completionHandler: @escaping () -> Void) {
+//        let id = response.notification.request.identifier
+//        NSLog("User tapped notification id: %@", id)
+//        // Можно прочитать response.actionIdentifier или response.notification.request.content.userInfo
+//        completionHandler()
+//    }
+}
